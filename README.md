0255da944b1241f451e1848c69e6f1e38261116f
electric131 <39623923+Electric131@users.noreply.github.com>
2025-02-21 18:01:10 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sandustry Light Spreader
+
+This is a tool to help map makers for Sandustry.
+
+## Usage
+
+Simply download or build the executable, and then place it in the same folder as your `map_blueprint_playtest.png`.
+
+Then run the script, wait a few seconds, and your `fog_playtest.png` should be placed in the same folder!
+
+## Building
+
+If you want to build the executable yourself, simply install the node modules then run one of the build scripts.
---
+++ index.js
@@ -0,0 +1,79 @@
+const fs = require("fs");
+const PNG = require("pngjs").PNG;
+
+const fadeDistance = 16;
+
+function handleImage() {
+	const image = this;
+	const mapData = image.data;
+	const fogData = Buffer.alloc(mapData.length).fill(0);
+	for (let i = 0; i < fogData.length; i += 4) {
+		fogData[i + 3] = fadeDistance; // Make alpha channel opaque
+	}
+
+	function getIndex(x, y) {
+		return (image.width * y + x) << 2;
+	}
+
+	function isValid(x, y) {
+		return !(x < 0 || y < 0 || x >= image.width || y >= image.height);
+	}
+
+	function pixelIsSurface(x, y) {
+		const colors = ["255, 255, 255", "102, 0, 255"];
+		const baseIndex = getIndex(x, y);
+		return colors.includes(`${mapData[baseIndex]}, ${mapData[baseIndex + 1]}, ${mapData[baseIndex + 2]}`);
+	}
+
+	let activePositions = [];
+	function floodFill() {
+		const offsets = [
+			[-1, 0],
+			[1, 0],
+			[0, -1],
+			[0, 1],
+		];
+		while (activePositions.length > 0) {
+			const [x, y] = activePositions.pop().split(", ").map(Number);
+			const index = getIndex(x, y);
+			if (pixelIsSurface(x, y)) {
+				fogData[index + 3] = 0;
+			}
+			for (const offset of offsets) {
+				const [newX, newY] = [x + offset[0], y + offset[1]];
+				if (isValid(newX, newY)) {
+					const baseIndex = getIndex(newX, newY);
+					// If adjacent pixel can become closer to the surface than it is
+					if (fogData[baseIndex + 3] > fogData[index + 3] + 1 && fogData[index + 3] + 1 < fadeDistance) {
+						fogData[baseIndex + 3] = fogData[index + 3] + 1;
+						activePositions.push(`${newX}, ${newY}`);
+					}
+				}
+			}
+		}
+	}
+
+	for (let y = 0; y < image.height; y++) {
+		for (let x = 0; x < image.width; x++) {
+			if (pixelIsSurface(x, y) && fogData[getIndex(x, y) + 3] == fadeDistance) {
+				activePositions.push(`${x}, ${y}`);
+				floodFill();
+			}
+		}
+	}
+
+	for (let i = 0; i < fogData.length; i += 4) {
+		fogData[i + 3] = fogData[i + 3] * (255 / fadeDistance); // Make alpha channel opaque
+	}
+
+	fs.writeFileSync(
+		"fog_playtest.png",
+		PNG.sync.write({
+			width: image.width,
+			height: image.height,
+			data: fogData,
+		})
+	);
+}
+
+fs.createReadStream("map_blueprint_playtest.png").pipe(new PNG()).on("parsed", handleImage);
